3d2aa1b2533503e1bffa2819c3ee987526077f6f
alexis-val <137000504+alexis-val@users.noreply.github.com>
2023-06-24 14:01:53 +0000

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_24_110639) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_110813) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -20,4 +20,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_24_110639) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.boolean "is_admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
